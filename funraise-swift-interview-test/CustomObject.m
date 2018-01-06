6a6ceeab79c3ecb2611af8900c583762efc1f548
Jason Swenski <jswenski@gmail.org>
2018-01-05 23:21:14 -0700

    rm spurious object

--- funraise-swift-interview-test/CustomObject.m
+++
@@ -1,9 +0,0 @@
-//
-//  CustomObject.m
-//  funraise-swift-interview-test
-//
-//  Created by Jason M Swenski on 1/5/18.
-//  Copyright © 2018 Funraise. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
