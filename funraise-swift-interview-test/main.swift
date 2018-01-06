4ba609a5e337470e823a4939a097570382c31718
Jason Swenski <jswenski@gmail.org>
2018-01-05 22:38:31 -0700

    Initial Commit

---
+++ funraise-swift-interview-test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  funraise-swift-interview-test
+//
+//  Created by Jason M Swenski on 1/5/18.
+//  Copyright © 2018 Funraise. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
